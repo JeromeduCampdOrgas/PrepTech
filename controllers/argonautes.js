2b6af1320d7332678ca29978c1d68e71cfcdcb3c
Jerome <ducampdorgasjerome@gmail.com>
2022-02-23 11:25:29 +0100

    update argonaute

--- controllers/argonautes.js
+++ controllers/argonautes.js
@@ -1,11 +1,52 @@
 const models = require("../models");
 
 exports.createArgonaute = (req, res) => {
-  console.log(req.body);
   const newArgonaute = models.Argonautes.create({
     name: req.body.name,
   });
   res.status(201).json({
     message: `Votre compte est bien créé ${newArgonaute.name} !`,
   });
+};
+exports.getAllArgonautes = async (req, res) => {
+  // on envoie tous les users sauf admin
+  try {
+    const argonautes = await models.Argonautes.findAll({
+      attributes: ["name", "id"],
+    });
+    res.status(200).send(argonautes);
+  } catch (error) {
+    return res.status(500).send({ error: "Erreur serveur" });
+  }
+};
+exports.deleteArgonaute = (req, res) => {
+  try {
+    const id = req.params.id;
+    {
+      models.Argonautes.destroy({ where: { id: id } }); // on supprime le compte
+      res.status(200).json({ messageRetour: "Argonautesupprimé" });
+    }
+  } catch (error) {
+    return res.status(500).send({ error: "Erreur serveur" });
+  }
+};
+exports.updateArgonaute = (req, res) => {
+  const id = req.params.id;
+
+  models.Argonautes.update(
+    {
+      name: req.body.name,
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  )
+    .then((argonaute) => {
+      res
+        .status(201)
+        .json({ message: "Argonaute successfully updated", argonaute });
+    })
+    .catch((err) => console.log(err));
 };
--- routes/argonautes.js
+++ routes/argonautes.js
@@ -2,9 +2,8 @@ const router = require("express").Router();
 const argoCtrl = require("../controllers/argonautes");
 
 router.post("/create", argoCtrl.createArgonaute);
-/*router.get("/argonautes", argoCtrl.getAllArgonautes); 
+router.get("/argonautes", argoCtrl.getAllArgonautes);
+router.delete("/argonaute/:id", argoCtrl.deleteArgonaute);
 router.put("/argonaute/:id", argoCtrl.updateArgonaute);
-router.get("/argonaute/:id", argoCtrl.getArgonaute); //auth,
-router.delete("/argonaute/:id",  argoCtrl.deleteArgonaute);*/
 
 module.exports = router;
